6e26ea53f9f55f8cbcc73a079d677390fddd89df
YakMM <yak.mayam@gmail.com>
2023-06-24 15:02:28 +0200

    Add paragon keyboard

---
+++ keyboards/artemis/paragon/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x47, 0x84, 0x44, 0x55, 0x1F, 0xD5, 0x19, 0xD1}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
